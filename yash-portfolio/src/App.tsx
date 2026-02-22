03174fdda641ce422aeb1d6ca8cd8ae6313b7ba6
Yashshakya1 <yashshakya787@gmail.com>
2026-02-22 06:29:06 +0530

    some update

--- yash-portfolio/src/App.tsx
+++ yash-portfolio/src/App.tsx
@@ -1,6 +1,5 @@
 
-import React, { useState, useEffect } from "react";
-import { motion } from "framer-motion";
+import { useState, useEffect } from "react";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
