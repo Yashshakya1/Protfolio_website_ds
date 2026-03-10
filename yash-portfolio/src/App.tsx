b24fe5ab2a3168d0b962b8a2c9019d10135f2443
Yashshakya1 <yashshakya787@gmail.com>
2026-03-10 23:35:16 +0530

    some update

--- yash-portfolio/src/App.tsx
+++ yash-portfolio/src/App.tsx
@@ -6,6 +6,7 @@ import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Contact from "./components/Contact";
 import Navigation from "./components/Navigation";
+// import "./App.css";
 
 export default function Portfolio() {
   const [activeSection, setActiveSection] = useState("hero");
